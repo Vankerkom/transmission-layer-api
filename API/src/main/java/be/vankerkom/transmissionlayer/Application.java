29f3074c375afbc716cfd53cea508ff2f07cd7dd
Vankerkom <daan@vankerkom.be>
2018-03-25 04:26:54 +0200

    Add CORS support

--- API/src/main/java/be/vankerkom/transmissionlayer/Application.java
+++ API/src/main/java/be/vankerkom/transmissionlayer/Application.java
@@ -2,11 +2,26 @@ package be.vankerkom.transmissionlayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @ComponentScan
 public class Application {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").
+                        allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
