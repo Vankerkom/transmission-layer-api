4039c3c2cda726eee7bae5ecfcbf44f0e7be9721
Daan Vankerkom <daan@vankerkom.be>
2018-12-10 22:37:11 +0100

    Move CORS to WebSecurityConfig

--- src/main/java/be/vankerkom/transmissionlayer/Application.java
+++ src/main/java/be/vankerkom/transmissionlayer/Application.java
@@ -11,19 +11,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@ComponentScan
 public class Application {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").
-                        allowedOrigins("http://localhost:4200");
-            }
-        };
-    }
 
     @Bean
     public ModelMapper modelMapper() {
--- src/main/java/be/vankerkom/transmissionlayer/config/WebSecurityConfig.java
+++ src/main/java/be/vankerkom/transmissionlayer/config/WebSecurityConfig.java
@@ -2,12 +2,15 @@ package be.vankerkom.transmissionlayer.config;
 
 import be.vankerkom.transmissionlayer.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -35,5 +38,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userService)
                 .passwordEncoder(passwordEncoder);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").
+                        allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+
 
 }
