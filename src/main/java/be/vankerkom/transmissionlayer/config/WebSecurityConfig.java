c875f476e344a3cd797c1b0d688b55bed63bc0dc
Daan Vankerkom <daan@vankerkom.be>
2018-12-14 00:32:08 +0100

    Add simple CORS and preflight support
    Allowed all CORS requests from localhost:4200 to speed-up development.
    Allowed options by default to speed-up development.

--- src/main/java/be/vankerkom/transmissionlayer/config/WebSecurityConfig.java
+++ src/main/java/be/vankerkom/transmissionlayer/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import be.vankerkom.transmissionlayer.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,9 +25,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.exceptionHandling()
-                .and()
-                .authorizeRequests() .anyRequest().authenticated()
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll() // Allow pre-flight requests.
+                .anyRequest()
+                .authenticated()
                 .and()
                 .httpBasic();
 
