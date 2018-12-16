0d3d1af58dca664f8fb9862c82f13ba73f5c3ab7
Daan Vankerkom <daan@vankerkom.be>
2018-12-16 20:56:59 +0100

    Fix exception error codes
    Added correct HTTP status codes to custom exceptions.

--- src/main/java/be/vankerkom/transmissionlayer/exceptions/DuplicateException.java
+++ src/main/java/be/vankerkom/transmissionlayer/exceptions/DuplicateException.java
@@ -1,7 +1,10 @@
 package be.vankerkom.transmissionlayer.exceptions;
 
 import be.vankerkom.transmissionlayer.models.dto.partials.TorrentDataDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicateException extends Exception {
 
     public DuplicateException(TorrentDataDto torrentData) {
--- src/main/java/be/vankerkom/transmissionlayer/exceptions/EntityNotFoundException.java
+++ src/main/java/be/vankerkom/transmissionlayer/exceptions/EntityNotFoundException.java
@@ -1,6 +1,14 @@
 package be.vankerkom.transmissionlayer.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException() {
+        super();
+    }
 
     public EntityNotFoundException(final String message) {
         super(message);
