571844765f3404d053e7d55288fcf23fcbc13b2b
Daan Vankerkom <daan@vankerkom.be>
2019-09-07 21:58:57 +0200

    Optimize imports

--- src/main/java/be/vankerkom/transmissionlayer/Application.java
+++ src/main/java/be/vankerkom/transmissionlayer/Application.java
@@ -1,14 +1,7 @@
 package be.vankerkom.transmissionlayer;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
